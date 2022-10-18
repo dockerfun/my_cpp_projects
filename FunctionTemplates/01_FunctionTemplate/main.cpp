7554e621a42b9ee491b15cfd1b9086c88c91cb9b
dockerfun <hmiklosazure@gmail.com>
2022-10-18 10:34:55 +0200

    function template example updated

--- FunctionTemplates/01_FunctionTemplate/main.cpp
+++ FunctionTemplates/01_FunctionTemplate/main.cpp
@@ -4,30 +4,51 @@ using namespace std;
 //prototype
 //blueprint for a max funtion
 template <typename T> T maximum(T a, T b);
+template <typename T> T multiply(T a, T b);
 
 int main(int argc, char **argv){
     
     /* code */
-    int a{10};
-    int b{23};
+    // int a{10};
+    // int b{23};
 
-    cout << "max(int): " << maximum(a, b) << endl; 
+    // cout << "max(int): " << maximum(a, b) << endl; 
 
-    double c{4.3};
-    double d{6.75};
+    // double c{4.3};
+    // double d{6.75};
 
-    cout << "max(double): " << maximum(c, d) << endl;
+    // cout << "max(double): " << maximum(c, d) << endl;
 
-    string e {"Hello"};
-    string f{"HelloWorld"};
+    // string e {"Hello"};
+    // string f{"HelloWorld"};
 
-    cout << "max(string): " << maximum(e,f) << endl;
+    // cout << "max(string): " << maximum(e,f) << endl;
 
+    int x{5};
+    int y{7};
+
+    int *p_x = {&x};
+    int *p_y = {&y};
+
+    auto result = maximum(p_x, p_y);
+    cout << "result: " << *result << endl;//5! incorrect, because the memory addresses were compared
+    //multiply(e, f); //build error with string
+
+    // double g{10.2};
+    // int h{7};
+
+    // multiply(g, h); //error with different types
     
+
     return 0;
 }
 
 template <typename T> 
 T maximum(T a, T b){
     return (a>b)?a:b;
+}
+
+template <typename T> 
+T multiply(T a, T b){
+    return a*b;
 }
