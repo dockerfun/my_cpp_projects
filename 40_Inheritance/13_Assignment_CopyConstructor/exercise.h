38cb2ec1a17069cae133aa85ed77345b056caab6
dockerfun <hmiklosazure@gmail.com>
2022-10-25 11:16:07 +0200

    copy constructors and assigment

---
+++ 40_Inheritance/13_Assignment_CopyConstructor/exercise.h
@@ -0,0 +1,44 @@
+#ifndef EXERCISE_H
+#define EXERCISE_H
+
+#include <iostream>
+
+class One {
+	public : 
+	One(int one ) : m_1 (one){
+	}
+
+	One(const One& source):m_1(source.m_1)  {}
+
+	protected : 
+		int m_1;
+};
+
+class Two : public One{
+	public : 
+	Two(int one, int two) : One(one), m_2(two) {
+	}
+	
+	Two(const Two& source):One(source), m_2(source.m_2)  {}
+	
+	protected : 
+		int m_2;
+};
+
+class Three : public Two{
+	public : 
+	Three( int one, int two, int three) : Two(one,two) , m_3(three){
+	}
+	//Copy constructor
+	Three(const Three& source):Two(source), m_3(source.m_3) {}
+
+	void print() const{
+		std::cout <<"data [one:" << m_1
+				 << " two:" << m_2 
+				 << " three:" << m_3 << "]" ;
+	}
+	protected: 
+		int m_3;
+};
+
+#endif // _EXERCISE_H
