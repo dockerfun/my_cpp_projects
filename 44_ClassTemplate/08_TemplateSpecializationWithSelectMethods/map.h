b1ee76410d6a1a417ca2cfed5ebec626e6a06076
dockerfun <hmiklosazure@gmail.com>
2022-10-31 21:56:41 +0100

    Template specialization with select methods

---
+++ 44_ClassTemplate/08_TemplateSpecializationWithSelectMethods/map.h
@@ -0,0 +1,20 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <string>
+#include <string_view>
+#define __P template <typename T, typename P>
+
+__P
+class Map{
+public:
+    Map(T key, P value)
+        : m_key(key), m_value(value){}
+    T m_key{};
+    P m_value{};
+    P get_value(){
+        return m_value;
+    }
+};
+
+#endif
