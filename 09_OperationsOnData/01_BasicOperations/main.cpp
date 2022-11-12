ef5e246a71180000f539246ffe1efb6a01dd62d3
dockerfun <hmiklosazure@gmail.com>
2022-11-12 16:00:38 +0100

    Basic operations example

---
+++ 09_OperationsOnData/01_BasicOperations/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    /* code */
+    // Addition
+    int number1(2);
+    int number2(4);
+
+    int sum { number1 + number2 };
+    int other_sum = number1 + number2 + number1;
+
+    std::cout << "The sum is: " << sum << std::endl;
+    std::cout << "Other sum is: " << other_sum << std::endl;
+
+    // Substraction
+    // Multiplication
+    // Division
+    // Modulus
+    
+    return 0;
+}
