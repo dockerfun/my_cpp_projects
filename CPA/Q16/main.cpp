8453e8f595277f4ec1a283ec5d13e5df57545ac9
dockerfun <hmiklosazure@gmail.com>
2022-11-12 12:01:45 +0100

    Question 16

---
+++ CPA/Q16/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+using namespace std; 
+
+class A { 
+    public: static int a; 
+    A() { 
+        a++; 
+    } 
+}; 
+
+int A::a = 1; 
+
+void f(void) { 
+    A a; 
+    throw string("?"); 
+} 
+
+int main(void) { 
+    A a; 
+    try { 
+        f(); 
+    } catch (string &s) { } 
+    cout << A::a << endl; //3
+    return 0; 
+}
