0ce34352cb59cb6e5e85c9b2e66058721b4723e3
dockerfun <hmiklosazure@gmail.com>
2022-11-27 21:10:07 +0100

    Quetion 5

---
+++ ITExams_Examples/Q5/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+template<class A>
+void f(A a)
+{
+    cout<<1<<endl;
+}
+void f(int a)
+{
+    cout<<2<<endl;
+}
+int main()
+{
+    int a = 1;
+    f<float>(a);
+
+    return 0;
+}
