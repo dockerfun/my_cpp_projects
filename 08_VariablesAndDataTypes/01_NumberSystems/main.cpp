811f65c1565b1f453756b5bc3850aff505b5a82f
dockerfun <hmiklosazure@gmail.com>
2022-12-30 16:05:00 +0100

    Number systems example

--- 08_VariablesAndDataTypes/01_NumberSystems/main.cpp
+++ 08_VariablesAndDataTypes/01_NumberSystems/main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 int main(){
     
     /* code */
+    int number1 = 15; //decimal 
+    int number2 = 017;//octal
+    int number3 = 0x0f;//hexadecimal
+    int number4 = 0b00001111; //binary - C++14
+
+    std::cout << "Hello World!" << std::endl;
+
+    std::cout << "Number1 is : " << number1 << std::endl;
+    std::cout << "Number2 is : " << number2 << std::endl;
+    std::cout << "Number3 is : " << number3 << std::endl;
+    std::cout << "Number4 is : " << number4 << std::endl;
+    
     
     return 0;
 }
