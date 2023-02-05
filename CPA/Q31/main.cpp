9fe58097c008e76f479b15c1bdb0b0219d7d926e
dockerfun <hmiklosazure@gmail.com>
2023-02-05 14:43:23 +0100

    few example projects added

---
+++ CPA/Q31/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    /* code */
+    //Floating point division by zero doesn’t throw an exception
+    cout << 4/0. << endl; //inf
+    
+    return 0;
+}
