b6bf1d2394f2294eb6f0d7d027ed03278cb7e84f
dockerfun <hmiklosazure@gmail.com>
2023-02-15 14:46:08 +0100

    Vector example

---
+++ CPP/03_Vector/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    /* code */
+    vector<int> v1(10);
+    vector<int> v2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (unsigned i = 0; i < v1.size(); ++i)
+    {
+        v1[i] = i;
+    }
+
+    for (unsigned i = 0; i < v1.size(); ++i)
+    {
+        cout << v1[i] << " ";
+        cout << v2[i] << endl;
+    }
+    cout << endl;
+
+    cout << "v2.size(): " << v2.size() << endl;
+    v1.push_back(100);
+
+    cout << "v1.size(): " << v1.size() << endl;
+
+    v1.pop_back();
+    cout << "v1.size(): " << v1.size() << endl;
+
+    return 0;
+}
