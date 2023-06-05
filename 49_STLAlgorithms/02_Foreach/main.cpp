99737aff335fce7753e18f03dfaa4fffb3349ae9
dockerfun <hmiklosazure@gmail.com>
2023-06-05 11:05:21 +0200

    Foreach example modified

--- 49_STLAlgorithms/02_Foreach/main.cpp
+++ 49_STLAlgorithms/02_Foreach/main.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+struct Sum
+{
+    void operator()(int n) { sum += n; }
+    int sum{0};
+};
+
 int main()
 {
 
@@ -22,11 +28,23 @@ int main()
     std::cout << "---------------------------------------" << std::endl;
 
     // Predicate that modifies elements in place
-    std::for_each(std::begin(nums), std::end(nums), [](int &n){ n++; });
+    std::for_each(std::begin(nums), std::end(nums), [](int &n)
+                  { n++; });
 
-    //Print
+    // Print
     std::for_each(std::begin(nums), std::end(nums), print);
     std::cout << std::endl;
+
+    // Capturing result through stateful functor that's returned
+    // calls Sum::operator() for each number
+    Sum s;
+    s = std::for_each(std::begin(nums), std::end(nums), s);
+    std::cout << "result : " << s.sum << std::endl;
+
+    //using lambda that captures a local variables by ref and modifies it
+    int our_result{0};
+    std::for_each(std::begin(nums), std::end(nums), [&our_result](int n){ our_result += n; });
+    std::cout << "result : " << our_result << std::endl;
 
     return 0;
 }
